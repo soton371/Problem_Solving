7940942da21901bfdc080196ca7e70404643daa2
Soton <soton371@gmail.com>
2024-03-23 08:10:50 +0600

    maxnumber

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from swap_the_first_and_last_elements_of_a_list import swap_first_last
+from maxnumber import max_number
 
-original_list = [12, 35, 9, 56, 24]
-swapped_list = swap_first_last(original_list)
-print(swapped_list)
+print(max_number(2, 4))
---
+++ maxnumber.py
@@ -0,0 +1,3 @@
+def max_number(a, b):
+    maxValue = max(a, b)
+    return maxValue
